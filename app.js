190eff6e0c93d256bc18a52406a21cde3391ae1b
Amélie Chabrand <amelie.chabrand@gmail.com>
2022-11-02 17:51:26 +0100

    all iterations done + bonus 5 + part of bonus 7

--- app.js
+++ app.js
@@ -14,12 +14,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { style: ['index.css'] });
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', async (req, res) => {
+  const rawResponse = await fetch('https://api.punkapi.com/v2/beers');
+  const data = await rawResponse.json();
+  const beers = data;
+  res.render('beers', { beers: beers, style: ['beers.css'] });
+});
+
+app.get('/random-beer', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom();
+  res.render('random-beer', {
+    randomBeer: randomBeer,
+    style: ['random-beer.css']
+  });
+});
+
+app.listen(3000, () => console.log('http://localhost:3000'));
